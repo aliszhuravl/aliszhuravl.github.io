78aae82d14f3f97dc1d44b5371a0a4ac7823e61d
aliszhuravl <alis.zhuravl@gmail.com>
2017-12-22 14:29:12 +0300

    all

--- js/main.js
+++ js/main.js
@@ -346,18 +346,23 @@ $('.dd_menu').each(function () {
     // Cache the number of options
     var $dropdown = $(this),
         $dropdowns = $('.dd_menu').not(this),
-        $dropdownList = $dropdown.find('.dropdown__list');
+        $dropdownList = $dropdown.find('.dropdown__list'),
+        $dropdownText = $dropdown.find('.dropdown__text');
+        $dropdownsText = $dropdowns.find('.dropdown__text');
 
     // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
     $dropdown.on('mouseover', function(e) {
         e.stopPropagation();
         $dropdowns.removeClass('dropdown_opened');
         $dropdowns.find('.dropdown__list').fadeOut(250);
+        $dropdownsText.removeClass('red');
 
         if ($dropdown.hasClass('dropdown_opened')) {
+            $dropdownText.addClass('red');
 
         } else {
             $dropdown.addClass('dropdown_opened');
+            $dropdownText.removeClass('red');
             $dropdown.find('.dropdown__list').css('display', 'flex').addClass('dl_open');
         }
     });
@@ -366,11 +371,14 @@ $('.dd_menu').each(function () {
         e.stopPropagation();
         $dropdowns.removeClass('dropdown_opened');
         $dropdowns.find('.dropdown__list').fadeOut(250);
+        $dropdownText.removeClass('red');
 
         if ($dropdown.hasClass('dropdown_opened')) {
+            $dropdownText.addClass('red');
 
         } else {
             $dropdown.addClass('dropdown_opened');
+            $dropdownText.addClass('red');
             $dropdown.find('.dropdown__list').css('display', 'flex').addClass('dl_open');
         }
     });
@@ -379,6 +387,7 @@ $('.dd_menu').each(function () {
     $(document.body).click(function() {
         $dropdown.removeClass('dropdown_opened');
         $dropdownList.fadeOut(250);
+        $dropdownText.removeClass('red');
     });
 
 });
